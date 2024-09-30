ec3f474de3f1ee50b2c6a05ee9024b4d84db39e5
Noveled <noveled@naver.com>
2024-09-30 16:10:48 +0900

    날씨 api 추가

--- weather.py
+++ weather.py
@@ -6,24 +6,26 @@ import io
 import os
 from langchain_community.document_loaders import WebBaseLoader
 
+os.environ['USER_AGENT'] = 'myagent'
+
 # 한글 출력 인코딩 설정 (환경에 따라 필요할 수 있음)
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 
 # 환경 변수에서 User-Agent 가져오기
-user_agent = os.getenv('USER_AGENT', 'Mozilla/5.0')
+# user_agent = os.getenv('USER_AGENT', 'Mozilla/5.0')
 
-# 요청 헤더에 User-Agent 설정 (여기서는 Chrome을 사용하는 것처럼 설정)
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-}
+# # 요청 헤더에 User-Agent 설정 (여기서는 Chrome을 사용하는 것처럼 설정)
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+# }
 # 여러 개의 URL 지정 (리스트로 설정)
 urls = ["https://blog.langchain.dev/customers-podium/", "https://blog.langchain.dev/langgraph-v0-2/"]
 
 # WebBaseLoader로 HTML 문서를 로드하면서 특정 부분만 파싱
+# header_template=headers,
 loader = WebBaseLoader(
     web_paths=urls,
-    header_template=headers,
     bs_kwargs=dict(
         parse_only=bs4.SoupStrainer(
             class_=("article-header", "article-content")  # 가져오려는 특정 클래스 선택
