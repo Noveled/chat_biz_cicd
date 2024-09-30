2ba9abfa6bad7298918a16cd030869a25538685a
Noveled <noveled@naver.com>
2024-09-30 16:04:24 +0900

    날씨 api 추가

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 console.log(path.join(__dirname));
 
 
-os.environ['USER_AGENT'] = 'myagent'
+// os.environ['USER_AGENT'] = 'myagent'
 // 포트 설정
 const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
